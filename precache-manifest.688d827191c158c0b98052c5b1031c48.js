40ffde29e1c8759125eb466a427f5774ca89b19d
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-09-15 19:44:32 +0530

    Updates

---
+++ precache-manifest.688d827191c158c0b98052c5b1031c48.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b80d81891273e3e99a680c7dbd4f3db",
+    "url": "/SiteX/index.html"
+  },
+  {
+    "revision": "33d4f392e561f07cec85",
+    "url": "/SiteX/static/css/main.3efbdd02.chunk.css"
+  },
+  {
+    "revision": "a4e10f1166657b120bcb",
+    "url": "/SiteX/static/js/2.a1783f1f.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/SiteX/static/js/2.a1783f1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d4f392e561f07cec85",
+    "url": "/SiteX/static/js/main.85f1157b.chunk.js"
+  },
+  {
+    "revision": "8fd4f7eb908f05570b75",
+    "url": "/SiteX/static/js/runtime-main.12bdc2ef.js"
+  },
+  {
+    "revision": "e6f1536247388eb1fffb88fbac7e85c9",
+    "url": "/SiteX/static/media/resume.e6f15362.pdf"
+  },
+  {
+    "revision": "72ad4b279cff2af936dec436d3e13ce8",
+    "url": "/SiteX/static/media/sangee.72ad4b27.jpg"
+  },
+  {
+    "revision": "52e5a3683eee7661bcc724a96d03df79",
+    "url": "/SiteX/static/media/simcentric.52e5a368.jpg"
+  }
+]);
